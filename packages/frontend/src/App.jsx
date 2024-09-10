9672a03abda87c4ae32d9b65fb5311539139abb1
carolcampos22 <mendes.carolina2209@gmail.com>
2024-09-09 22:09:26 -0300

    feat(frontend): add login page and integrate GraphQL authentication

---
+++ packages/frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Login from './pages/Login/Login';
+import Welcome from './pages/Welcome/Welcome';
+
+const App = () => (
+  <Routes>
+    <Route path="/" element={<Login />} />
+    <Route path="/welcome" element={<Welcome />} />
+  </Routes>
+);
+
+export default App;
+
